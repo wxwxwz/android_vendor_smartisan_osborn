b7547a93f73c9780e77d15e20040ee35e74c1907
anrui2032 <anrui2032@gmail.com>
2023-03-26 12:33:22 +0000

    osborn: Update QMI from jasmine V11.0.17.0.QDIMIXM
    Change-Id: Ia53c7a48ee47efe01b609c2f74545d4e23bdd026

--- osborn-vendor.mk
+++ osborn-vendor.mk
@@ -537,6 +537,7 @@ PRODUCT_COPY_FILES += \
     vendor/smartisan/osborn/proprietary/product/lib64/com.qualcomm.qti.dpm.api@1.0.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/com.qualcomm.qti.dpm.api@1.0.so \
     vendor/smartisan/osborn/proprietary/product/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptXHD_encoder.so \
     vendor/smartisan/osborn/proprietary/product/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libaptX_encoder.so \
+    vendor/smartisan/osborn/proprietary/product/lib64/libdiag_system.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdiag_system.so \
     vendor/smartisan/osborn/proprietary/product/lib64/libdpmctmgr.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdpmctmgr.so \
     vendor/smartisan/osborn/proprietary/product/lib64/libdpmfdmgr.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdpmfdmgr.so \
     vendor/smartisan/osborn/proprietary/product/lib64/libdpmframework.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libdpmframework.so \
